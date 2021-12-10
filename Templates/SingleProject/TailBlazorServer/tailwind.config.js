eb4b8a368c17e5ba620a73069c02f66c95985408
McNerdius <mcnerd@techie.com>
2021-12-10 08:57:32 -0700

    sync TailBlazorServer files

--- Templates/SingleProject/TailBlazorServer/tailwind.config.js
+++ Templates/SingleProject/TailBlazorServer/tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
     content: [
         './**/*.{razor,html,cshtml}'
     ],
+    theme:
+    {
+        extend:
+        {
+            colors: {
+                'dotnet_blurple': '#512BD4'
+            }
+        }
+    },
     darkMode: 'class',
     plugins: [
         require("@tailwindcss/typography"),
