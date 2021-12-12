9dd433c3d363f01dd41902ce3a96ed8ef22a32ea
McNerdius <924403+McNerdius@users.noreply.github.com>
2021-12-12 07:28:24 -0800

    initial multiproject commit
    still Razor Pages / MAUI to go

--- Templates/MultiProject/BlazorWasm/Program.cs
+++ Templates/MultiProject/BlazorWasm/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using TailBlazorWasm;
+using RazorClassLibrary;
 
 var builder = WebAssemblyHostBuilder.CreateDefault( args );
 builder.RootComponents.Add<App>( "#app" );
