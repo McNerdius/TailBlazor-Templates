db90f33f1514dc046b131bae969e1b0a61c3f832
McNerdius <mcnerd@techie.com>
2021-12-12 08:19:37 -0700

    initial multiproject commit

---
+++ Templates/MultiProject/RazorClassLibrary/Components/DarkSwitch/DarkSwitch.razor.cs
@@ -0,0 +1,30 @@
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace RazorClassLibrary.Components;
+
+public partial class DarkSwitch : ComponentBase
+{
+    [Inject] IJSRuntime JSRuntime { get; set; }
+
+    private Task<IJSObjectReference> module;
+    private Task<IJSObjectReference> Module => module ??= JSRuntime.InvokeAsync<IJSObjectReference>( "import", "./_content/RazorClassLibrary/Components/DarkSwitch/DarkSwitch.razor.js" ).AsTask();
+
+    private async Task switchTheme()
+    {
+        var module = await Module;
+        await module.InvokeAsync<object>( "toggleTheme" );
+
+    }
+
+    protected override async Task OnAfterRenderAsync( bool firstRender )
+    {
+        if ( firstRender )
+        {
+            var module = await Module;
+            await module.InvokeAsync<object>( "loadTheme" );
+        }
+        base.OnInitialized();
+    }
+}
