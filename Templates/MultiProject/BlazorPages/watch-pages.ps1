12c344311dc0c8e610fbad4e5ae8d8f424d7b2c6
McNerdius <mcnerd@techie.com>
2022-01-05 14:24:01 -0700

    correct watch script paths

--- Templates/MultiProject/BlazorPages/watch-pages.ps1
+++ Templates/MultiProject/BlazorPages/watch-pages.ps1
@@ -1,6 +1,6 @@
 start "dotnet" -ArgumentList "watch" # -NoNewWindow
 
-while (!(Test-Path "../RazorClassLibrary/obj/scopedcss/bundle/RazorClassLibrary.styles.css")) { sleep -ms 100 } # tailwind needs this file
+while (!(Test-Path "../RazorClassLibrary/obj/net6.0/scopedcss/bundle/RazorClassLibrary.styles.css")) { sleep -ms 100 } # tailwind needs this file
 while (!(Test-Path "../node_modules/.install-stamp")) { sleep -ms 100 } # npm run needs this file 
 
 npm --prefix ../ run watch
--- Templates/SingleProject/TailBlazorPages/watch.ps1
+++ Templates/SingleProject/TailBlazorPages/watch.ps1
@@ -1,6 +1,6 @@
 start "dotnet" -ArgumentList "watch" # -nonewwindow
 
-while (!(Test-Path "./obj/scopedcss/bundle/TailBlazorPages.styles.css")) { sleep -ms 100 } # tailwind needs this file
+while (!(Test-Path "./obj/net6.0/scopedcss/bundle/TailBlazorPages.styles.css")) { sleep -ms 100 } # tailwind needs this file
 while (!(Test-Path "./node_modules/.install-stamp")) { sleep -ms 100 } # npm run needs this file 
 
 npm run watch
