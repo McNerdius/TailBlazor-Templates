3d2c81d774ae44438f50ed6347ef356fe6b57da5
McNerdius <mcnerd@techie.com>
2022-10-31 14:32:13 -0600

    remove hosted WIP

--- WIP/Hosted/watch.ps1
+++
@@ -1,6 +0,0 @@
-start "dotnet" -ArgumentList "watch run --project:Client" #  -nonewwindow
-
-while (!(Test-Path "./Client/obj/scopedcss/bundle/Client.styles.css")) { sleep -ms 100 } # tailwind needs this file
-while (!(Test-Path "./node_modules/.install-stamp")) { sleep -ms 100 } # npm run needs this file 
-
-npm run watch
