ab05296471eed5a6fe2f660f60444903080be54f
McNerdius <mcnerd@techie.com>
2023-02-21 07:42:59 -0700

    nuke

--- Templates/SingleProject/TailBlazorServer/wwwroot/initializeDarkMode.js
+++
@@ -1,8 +0,0 @@
-
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
-{
-    document.documentElement.classList.add('dark')
-} else
-{
-    document.documentElement.classList.remove('dark')
-}
